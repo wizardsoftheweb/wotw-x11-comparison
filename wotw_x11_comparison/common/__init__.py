1caccaa148dd0acb952d1d327f942a9efee9ce5f
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-10 08:30:31 -0600

    Add MovesMouse to module

--- wotw_x11_comparison/common/__init__.py
+++ wotw_x11_comparison/common/__init__.py
@@ -1,4 +1,5 @@
 """Placeholder"""
 from .has_logger import HasLogger
+from .moves_mouse import MovesMouse
 from .uses_xcb_window_properties import UsesXcbWindowProperties
 from .writes_results import WritesResults
