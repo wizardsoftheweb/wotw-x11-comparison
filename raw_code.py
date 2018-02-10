1faa4fbaf27b3c3b01cf7302db53bface539011a
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-09 22:24:35 -0600

    Add xcb_connect defs

--- raw_code.py
+++ raw_code.py
@@ -4,4 +4,21 @@ from __future__ import print_function
 
 from ctypes import *
 
-lib = CDLL('libxcb.so.1')
+xcb = CDLL('libxcb.so.1')
+
+DisplayName = c_char_p
+ScreenNumber = c_int
+
+
+class xcb_connection_t(Structure):
+    _fields_ = [
+        ('_opaque_struct', c_int)
+    ]
+
+xcb.xcb_connect.argtypes = [DisplayName, POINTER(ScreenNumber)]
+xcb.xcb_connect.restype = xcb_connection_t
+
+DEFAULT_SCREEN_NUMBER = ScreenNumber()
+CONNECTION = xcb.xcb_connect(None, byref(DEFAULT_SCREEN_NUMBER))
+print(CONNECTION)
+print(DEFAULT_SCREEN_NUMBER)
