ea1e22cd573df7ae673c8f252dafe762b7c9c69f
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-10 15:48:40 -0600

    Polish docstrings

--- wotw_x11_comparison/pointer_window/__init__.py
+++ wotw_x11_comparison/pointer_window/__init__.py
@@ -1,4 +1,4 @@
-"""Placeholder"""
+"""This file provides the pointer_window module"""
 from .base import BasePointerWindow
 from .using_xcb import XcbPointerWindow
 from .using_xlib import XlibPointerWindow
