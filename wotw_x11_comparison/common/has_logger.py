e5518a27f056fcc289131dea52c91109dea3e12c
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-10 07:05:20 -0600

    Create a basic logger

---
+++ wotw_x11_comparison/common/has_logger.py
@@ -0,0 +1,34 @@
+# pylint: disable=W,C,R
+from logging import addLevelName, Formatter, getLogger, INFO, StreamHandler
+from sys import exit as stderr
+
+SILLY = 5
+addLevelName(SILLY, 'SILLY')
+LOGGER = getLogger('wotw-x11-comparison')
+CONSOLE_HANDLER = StreamHandler(stream=stderr)
+CONSOLE_FORMATTER = Formatter(
+    '[%(asctime)s][%(name)s][%(levelname)s] %(message)s'
+)
+CONSOLE_HANDLER.setFormatter(CONSOLE_FORMATTER)
+LOGGER.addHandler(CONSOLE_HANDLER)
+LOGGER.silly = (
+    lambda message, *args, **kwargs:
+    LOGGER.log(SILLY, message, *args, **kwargs)
+)
+LOGGER.setLevel(INFO)
+
+
+class HasLogger(object):
+
+    def __init__(
+            self,
+            logger_name='wotw-x11-comparison',
+            logger_level=INFO
+    ):
+        self.logger = getLogger(logger_name)
+        self.logger.addHandler(CONSOLE_HANDLER)
+        self.logger.silly(
+            lambda message, *args, **kwargs:
+            self.logger.log(SILLY, message, *args, **kwargs)
+        )
+        self.logger.setLevel(logger_level)
