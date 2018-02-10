cf82955f768dbc969b46b11259a0e12b3f7b5a0f
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-09 22:55:16 -0600

    Add xcb_setup_roots_iterator

--- raw_code.py
+++ raw_code.py
@@ -49,44 +49,45 @@ class xcb_setup_t(Structure):
     ]
 
 
-# class xcb_screen_t(Structure):
-#     _fields_ = [
-#         ('root', xcb_window_t),
-#         ('default_colormap', IGNORED_FOR_NOW),
-#         ('white_pixel', uint32_t),
-#         ('black_pixel', uint32_t),
-#         ('current_input_masks', uint32_t),
-#         ('width_in_pixels', uint16_t),
-#         ('height_in_pixels', uint16_t),
-#         ('width_in_millimeters', uint16_t),
-#         ('height_in_millimeters', uint16_t),
-#         ('min_installed_maps', uint16_t),
-#         ('max_installed_maps', uint16_t),
-#         ('root_visual', IGNORED_FOR_NOW),
-#         ('backing_stores', uint8_t),
-#         ('save_unders', uint8_t),
-#         ('root_depth', uint8_t),
-#         ('allowed_depths_len', uint8_t),
-#     ]
+class xcb_screen_t(Structure):
+    _fields_ = [
+        ('root', xcb_window_t),
+        ('default_colormap', IGNORED_FOR_NOW),
+        ('white_pixel', uint32_t),
+        ('black_pixel', uint32_t),
+        ('current_input_masks', uint32_t),
+        ('width_in_pixels', uint16_t),
+        ('height_in_pixels', uint16_t),
+        ('width_in_millimeters', uint16_t),
+        ('height_in_millimeters', uint16_t),
+        ('min_installed_maps', uint16_t),
+        ('max_installed_maps', uint16_t),
+        ('root_visual', IGNORED_FOR_NOW),
+        ('backing_stores', uint8_t),
+        ('save_unders', uint8_t),
+        ('root_depth', uint8_t),
+        ('allowed_depths_len', uint8_t),
+    ]
 
 
-# class xcb_screen_iterator_t(Structure):
-#     _fields_ = [
-#         ('data', POINTER(xcb_screen_t)),
-#         ('rem', uint8_t),
-#         ('index', uint8_t),
-#     ]
+class xcb_screen_iterator_t(Structure):
+    _fields_ = [
+        ('data', POINTER(xcb_screen_t)),
+        ('rem', uint8_t),
+        ('index', uint8_t),
+    ]
 
 xcb.xcb_connect.argtypes = [DisplayName, POINTER(ScreenNumber)]
 xcb.xcb_connect.restype = POINTER(xcb_connection_t)
 xcb.xcb_get_setup.argtypes = [POINTER(xcb_connection_t)]
 xcb.xcb_get_setup.restype = POINTER(xcb_setup_t)
-# xcb.xcb_setup_roots_iterator.argtypes = [POINTER(xcb_setup_t)]
-# xcb.xcb_setup_roots_iterator.restype = xcb_screen_iterator_t
+xcb.xcb_setup_roots_iterator.argtypes = [POINTER(xcb_setup_t)]
+xcb.xcb_setup_roots_iterator.restype = xcb_screen_iterator_t
 # xcb.screen_of_display.argtypes = [xcb_connection_t, ScreenNumber]
 # xcb.screen_of_display.restype = xcb_screen_t
 
 DEFAULT_SCREEN_NUMBER = ScreenNumber()
 CONNECTION = xcb.xcb_connect(None, byref(DEFAULT_SCREEN_NUMBER))
 SETUP = xcb.xcb_get_setup(CONNECTION)
+ITERATOR = xcb.xcb_setup_roots_iterator(SETUP)
 # SCREEN = xcb.screen_of_display(CONNECTION, DEFAULT_SCREEN_NUMBER)
