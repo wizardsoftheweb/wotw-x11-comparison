dcd879049cb8a151d224f218e233941722bbbb8c
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-10 14:51:27 -0600

    Test get_mouse_windows

--- tests/pointer_window/test_using_xlib.py
+++ tests/pointer_window/test_using_xlib.py
@@ -77,3 +77,36 @@ class GetRootWindowUnitTests(XlibPointerWindowTestCase):
             self.ROOT_WINDOW,
             self.window.get_root_window(self.PRIMARY, self.SECONDARY)
         )
+
+
+class GetMouseWindowsUnitTests(XlibPointerWindowTestCase):
+
+    @patch(
+        'wotw_x11_comparison.pointer_window.using_xlib.xlib.XQueryPointer'
+    )
+    @patch(
+        'wotw_x11_comparison.pointer_window.using_xlib.Window',
+        return_value=MagicMock(
+            value=XlibPointerWindowTestCase.CHILD_WINDOW
+        )
+    )
+    @patch(
+        'wotw_x11_comparison.pointer_window.using_xlib.Coordinate'
+    )
+    @patch(
+        'wotw_x11_comparison.pointer_window.using_xlib.byref'
+    )
+    def test_calls(self, mock_byref, mock_coord, mock_window, mock_query):
+        mock_holder = MagicMock()
+        mock_holder.attach_mock(mock_window, 'Window')
+        mock_holder.attach_mock(mock_coord, 'Coordinate')
+        mock_holder.attach_mock(mock_byref, 'byref')
+        mock_holder.attach_mock(mock_query, ' XQueryPointer')
+        root, current = self.window.get_mouse_windows(
+            self.PRIMARY,
+            self.CHILD_WINDOW
+        )
+        self.assertEquals(root, current)
+        self.assertEquals(mock_window.call_count, 2)
+        self.assertEquals(mock_coord.call_count, 4)
+        self.assertEquals(mock_byref.call_count, 7)
